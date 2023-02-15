9600dd4975d269d0f5d9e5ae9a3a87c414a7ddb5
Oscar Vestlund <ove@lysator.liu.se>
2023-02-15 11:47:13 +0100

    tkinter interface: make interface threaded

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ except:
     print('''tkinter couldnt initialize, if you are using GNU/Linux try running any of these commands:\n"pip install tk"\n"pacman -S tk"\n"echo 'dev-lang/python tk' >> /etc/portage/package.use/python; emerge -uvDU @world"''')
     exit(1)
 
+class interface:
+    def __init__(self):
+        self.
+
 def main():
     window = tk.Tk() #create a tk window object
     window.title("Shopping List Assistant 3000") #heckin poggers innit?
 
-    side_panel = tk.Frame(master=window, width=200, height=400, bg="green")
+    side_panel = tk.Frame(master=window, width=200, height=400, bg="#303030")
     main_panel = tk.Frame(master=window, width=200, height=400, bg="black")
 
     window.mainloop()
