3bfa3d0ce37201cc370e6ad4a6ae42bddd067a1c
Oscar Vestlund <ove@lysator.liu.se>
2023-02-15 23:50:58 +0100

    main: copy curses version of main into main

--- main.py
+++ main.py
@@ -1,66 +1,79 @@
-from threading import Thread
-try:
-    import tkinter as tk
-except:
-    print('''tkinter couldnt initialize, if you are using GNU/Linux try running any of these commands:\n"pip install tk"\n"pacman -S tk"\n"echo 'dev-lang/python tk' >> /etc/portage/package.use/python; emerge -uvDU @world"''')
-    exit(1)
+import curses #this is for the cli - command line interface
+import json #this is to save and load shopping lists
+import threading #this makes the input crispier than pringles
+import queue #this is to send data between threads
 
-#interface class with non specific functors
-class interface_class(Thread):
-    sidebar_info = ""
-    shopping_list = ""
-    def set_sidebar_info(self, sidebar_info_to_set):
-        self.sidebar_info = sidebar_info_to_set
-    def set_shopping_list(self, shopping_list_to_set):
-        self.shopping_list = shopping_list_to_set
-
-#tk_interface based on interface class
-class tk_interface_class(interface_class):
+class input_thread_class(threading.Thread):
     def run(self):
-        self.window = tk.Tk()
-        self.window.title("Shopping List Assistant")
-        self.side_panel = tk.Frame(master=self.window, width=200, height=500, bg="#202020")
-        self.side_panel.pack(fill=tk.Y, side=tk.LEFT, expand=False)
-        self.main_panel = tk.Canvas(self.window, width=600, height=500, bg="black", highlightthickness=0)
-        self.main_panel.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-        self.main_panel.create_line(100, 100, 200, 50, fill = "#204090", width = 3)
-        self.window.bind('<KeyPress>', self.keypress_handler)
-        self.window.mainloop()
-        global action
-        action = "exit"
-    def keypress_handler(self, event):
-        global action
-        action = str(event.keysym)
-    def destroy_interface(self):
-        self.window.quit()
-        self.window.destroys()
+        key = "null" #initialize var and enter the loop
+        while key != "q":
+            key = interface.stdscr.getkey()
+            input_queue.put(key)
 
-action = "none"
+class interface_class():
+    def __init__(self):
+        self.stdscr = curses.initscr()
+        curses.cbreak()
+        curses.noecho()
+        curses.curs_set(0)
+        self.stdscr.clear()#clear the screen
+        self.stdscr.addstr("menu:     ")#10 chars long
+        self.stdscr.vline(curses.ACS_VLINE, 1000)
+        self.stdscr.refresh()
+        for option in menu.options:
+            self.stdscr.addstr(option)
+
+class shopping_list_class:
+    def __init__(self):
+        try: #if we can read a json from default.shoppinglist we won't put the default items eggs and bacon in the list
+            file_open = open("default.shoppinglist", "r")
+            json_string = read(file_open)
+            self.items = json.load(json_string)
+        except:
+            self.items = [["eggs", 12], ["bacon", 1]]
+        self.selected = 0
+
+#menu class
+class menu_class:
+    def __init__(self):
+        self.options = ["add", "remove", "read", "save"]
+        self.selected = 0
+
+#non functor functions
+def select_down(pane):
+    if pane == "menu" and menu.selected < len(menu.menu_list):
+        menu.selected += 1
+    elif pane == "shopping_list" and shopping_list.selected < len(shopping_list.items):
+        shopping_list.selected += 1
+
+def select_up(pane):
+    if pane == "menu" and menu.selected != 0:
+        menu.selected -= 1
 
 def main():
-    #start the interface
-    interface = tk_interface_class()
-    interface.start()
-
-    #setup variables
-    option_list = ["a = add", "d = delete", "j = down", "k = up", "q = quit"]
-
-    global action
-    for i in range(3000):
-        if action == "a":
-            print("add")
-        elif action == "q":
-            interface.destroy_interface()
+    input_thread.start()
+    selected_pane = "menu"
+    while True:
+        key = input_queue.get()
+        if key == "q":
             break
-        elif action == "exit": #someone pressed the x
-            print(action)
-            break
-        print(action)
-        action = "none"
+        elif key == "m":
+            move_up(selected_pane)
+        elif key == "n":
+            move_down(selected_pane)
+        elif key == "j":
+            select_down(selected_pane)
+        elif key == "k":
+            select_up(selected_pane)
+        elif key == "h":
+            selected_pane = "menu"
+        elif key == "l":
+            selected_pane = "shopping_list"
+    input_thread.join()#when the loop is broken we will wait for the interface thread to join the main thread
 
-    #this destroys the window and closes the program
-    print(action)
-    interface.join()
-    exit(0)
-
-main()
+menu = menu_class()#this is the menu class
+input_thread = input_thread_class()
+interface = interface_class()#the interface class (which is a thread)
+input_queue = queue.Queue()#this makes it possible to read and send data between threads
+shopping_list = shopping_list_class()#the actual shopping list
+main()#starts main
