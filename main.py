c61e83a7201094ac2cfec3fb20df7067616a4ef8
Oscar Vestlund <ove@lysator.liu.se>
2023-02-16 13:56:35 +0100

    add popup class for menu options

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class interface_class():
         curses.cbreak()
         curses.noecho()
         curses.curs_set(0)
+        self.rows, self.cols = self.stdscr.getmaxyx()
         self.stdscr.clear()#clear the screen
         self.print_menu()
         self.print_shopping_list()
@@ -51,7 +52,7 @@ class interface_class():
             if menu.selected == iterator:
                 self.stdscr.move(y_coord, x_coord - 1)
                 self.stdscr.addch(">")
-            self.stdscr.addstr(menu.options[iterator])
+            self.stdscr.addstr(menu.options[iterator][0])
         y_coord += 1
         self.stdscr.move(y_coord, 1)
         self.stdscr.hline(curses.ACS_HLINE, 15)
@@ -64,21 +65,51 @@ class interface_class():
 
 class shopping_list_class:
     def __init__(self):
-        #try: #if we can read a json from default.shoppinglist we won't put the default items eggs and bacon in the list
-        #    file_open = open("default.shoppinglist", "r")
-        #    json_string = read(file_open)
-        #    self.items = json.load(json_string)
-        #except:
-        #    self.items = [["eggs", 12], ["bacon", 1]]
-        self.items = [["eggs", 12], ["bacon", 1]]
+        try: #if we can read a json from default.shoppinglist we won't put the default items eggs and bacon in the list
+            file_open = open("default.shoppinglist", "r")
+            json_string = read(file_open)
+            self.items = json.load(json_string)
+        except:
+            self.items = [["eggs", 12], ["bacon", 1]]
+        self.items = [["eggs", 12], ["bacon", 1], ["toast", 1], ["eggplant", 2], ["carrots", 4]]
         self.selected = 0
 
 #menu class
 class menu_class:
     def __init__(self):
-        self.options = ["add", "load", "save", "sort list", "export to file", "from recipe"]
+        self.options = [["add", add_class()], "load", "save", ["sort list", sort_class()], "export to file", "from recipe"]
         self.legend = ["k: up", "j: down", "l: list", "h: menu", "m: move up", "n: move down", "q: quit"]
         self.selected = 0
+    def activate(self):
+        self.options[self.selected][1].activate_option()
+
+class option_class:
+    def set_coords(self):
+        return 0
+
+class sort_class(option_class):
+    def activate_option(self):
+        shopping_list.items.sort()
+        interface.print_shopping_list()
+
+class popup_class(option_class):
+    def __init__(self):
+        self.width = 40
+        self.height = 4
+        self.titel = "popup"
+        self.x_coord = 30 #this is just placeholders
+        self.y_coord = 6 #this is just placeholders
+    def set_coords(self):
+        self.x_coord = (interface.cols // 2) - (self.width // 2)
+        self.y_coord = (interface.rows // 2) - (self.height // 2)
+    def activate_popup(self):
+        interface.stdscr.move(self.y_coord, self.x_coord)
+        interface.stdscr.hline(curses.ACS_HLINE, self.width)
+        interface.stdscr.refresh()
+
+class add_class(popup_class):
+    def activate_option(self):
+        self.activate_popup()
 
 #non functor functions
 def move_down(pane):
@@ -130,6 +161,8 @@ def main():
             selected_pane = "menu"
         elif key == "l":
             selected_pane = "shopping_list"
+        elif key == " " or key == "\n":
+            menu.activate()
     input_thread.join()#when the loop is broken we will wait for the interface thread to join the main thread
 
 menu = menu_class()#this is the menu class
