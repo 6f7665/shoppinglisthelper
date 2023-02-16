b0d7c777422217c8ad20f1bccac62cf840be387c
Oscar Vestlund <ove@lysator.liu.se>
2023-02-16 13:12:01 +0100

    shopping_list: add move functionality and print function

--- main.py
+++ main.py
@@ -18,6 +18,24 @@ class interface_class():
         curses.curs_set(0)
         self.stdscr.clear()#clear the screen
         self.print_menu()
+        self.print_shopping_list()
+
+    def print_shopping_list(self):
+        y_coord = 0
+        x_coord = 18
+        self.stdscr.move(y_coord +1, x_coord - 1)
+        self.stdscr.vline(" ", len(shopping_list.items))
+        self.stdscr.move(y_coord, x_coord)
+        self.stdscr.addstr("shopping-list:")#15 chars long
+        for iterator in range(len(shopping_list.items)):
+            y_coord += 1
+            self.stdscr.move(y_coord, x_coord)
+            if shopping_list.selected == iterator:
+                self.stdscr.move(y_coord, x_coord - 1)
+                self.stdscr.addch(">")
+            self.stdscr.addnstr((shopping_list.items[iterator][0] + "                                                         "), (curses.COLS - 19))
+        y_coord += 1
+        self.stdscr.refresh()
 
     def print_menu(self):
         y_coord = 0
@@ -39,19 +57,20 @@ class interface_class():
         self.stdscr.hline(curses.ACS_HLINE, 15)
         for iterator in range(len(menu.legend)):
             y_coord += 1
-            self.stdscr.move(y_coord, 1)
+            self.stdscr.move(y_coord, x_coord)
             self.stdscr.addstr(menu.legend[iterator])
         self.stdscr.refresh()
 
 
 class shopping_list_class:
     def __init__(self):
-        try: #if we can read a json from default.shoppinglist we won't put the default items eggs and bacon in the list
-            file_open = open("default.shoppinglist", "r")
-            json_string = read(file_open)
-            self.items = json.load(json_string)
-        except:
-            self.items = [["eggs", 12], ["bacon", 1]]
+        #try: #if we can read a json from default.shoppinglist we won't put the default items eggs and bacon in the list
+        #    file_open = open("default.shoppinglist", "r")
+        #    json_string = read(file_open)
+        #    self.items = json.load(json_string)
+        #except:
+        #    self.items = [["eggs", 12], ["bacon", 1]]
+        self.items = [["eggs", 12], ["bacon", 1]]
         self.selected = 0
 
 #menu class
@@ -62,12 +81,27 @@ class menu_class:
         self.selected = 0
 
 #non functor functions
+def move_down(pane):
+    if pane == "shopping_list" and shopping_list.selected < (len(shopping_list.items) - 1):
+        item = shopping_list.items.pop(shopping_list.selected)
+        shopping_list.items.insert((shopping_list.selected + 1), item)
+        shopping_list.selected += 1
+        interface.print_shopping_list()
+
 def select_down(pane):
     if pane == "menu" and menu.selected < (len(menu.options) - 1):
         menu.selected += 1
         interface.print_menu()
     elif pane == "shopping_list" and shopping_list.selected < (len(shopping_list.items) - 1):
         shopping_list.selected += 1
+        interface.print_shopping_list()
+
+def move_up(pane):
+    if pane == "shopping_list" and shopping_list.selected != 0:
+        item = shopping_list.items.pop(shopping_list.selected)
+        shopping_list.items.insert((shopping_list.selected - 1), item)
+        shopping_list.selected -= 1
+        interface.print_shopping_list()
 
 def select_up(pane):
     if pane == "menu" and menu.selected != 0:
@@ -75,6 +109,7 @@ def select_up(pane):
         interface.print_menu()
     elif pane == "shopping_list" and shopping_list.selected != 0:
         shopping_list.selected -= 1
+        interface.print_shopping_list()
 
 def main():
     input_thread.start()
@@ -98,8 +133,8 @@ def main():
     input_thread.join()#when the loop is broken we will wait for the interface thread to join the main thread
 
 menu = menu_class()#this is the menu class
+shopping_list = shopping_list_class()#the actual shopping list
 input_thread = input_thread_class()
 interface = interface_class()#the interface class (which is a thread)
 input_queue = queue.Queue()#this makes it possible to read and send data between threads
-shopping_list = shopping_list_class()#the actual shopping list
 main()#starts main
